818264ffa0f0dce12370e3ec74eef3bc4e5e1cec
EnAnsari <rahmat.ansari.dev@gmail.com>
2025-08-03 12:50:57 +0330

    update: structure updated by adding .env file + Book entity

--- src/main/java/org/rahmasir/crudrestjpademo/CrudRestJpaDemoApplication.java
+++ src/main/java/org/rahmasir/crudrestjpademo/CrudRestJpaDemoApplication.java
@@ -1,5 +1,7 @@
 package org.rahmasir.crudrestjpademo;
 
+import io.github.cdimascio.dotenv.Dotenv;
+import io.github.cdimascio.dotenv.DotenvEntry;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +9,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CrudRestJpaDemoApplication {
 
     public static void main(String[] args) {
+
+        Dotenv dotenv = Dotenv.configure().filename(".env").load();
+        for (DotenvEntry entry : dotenv.entries()) {
+            if (System.getProperty(entry.getKey()) == null) {
+                System.setProperty(entry.getKey(), entry.getValue());
+            }
+        }
+
         SpringApplication.run(CrudRestJpaDemoApplication.class, args);
     }
-
 }
