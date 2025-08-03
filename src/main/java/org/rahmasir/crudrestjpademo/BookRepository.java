cad36762b5353dda2389505e712b6df0fb669285
EnAnsari <rahmat.ansari.dev@gmail.com>
2025-08-03 12:58:24 +0330

    create: a fundamental repository create

---
+++ src/main/java/org/rahmasir/crudrestjpademo/BookRepository.java
@@ -0,0 +1,12 @@
+package org.rahmasir.crudrestjpademo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface BookRepository extends JpaRepository<Book,Integer> {
+
+    List<Book> findByAuthor(String author);
+    List<Book> findByTitleContaining(String keyword);
+    List<Book> findByPriceBetween(double minPrice, double maxPrice);
+}
