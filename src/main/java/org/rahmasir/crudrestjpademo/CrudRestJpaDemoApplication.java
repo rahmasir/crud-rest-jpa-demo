c7f301857d554caf39a869ebad05891c16cdb63c
EnAnsari <rahmat.ansari.dev@gmail.com>
2025-08-03 09:42:55 +0330

    Initial commit

---
+++ src/main/java/org/rahmasir/crudrestjpademo/CrudRestJpaDemoApplication.java
@@ -0,0 +1,13 @@
+package org.rahmasir.crudrestjpademo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudRestJpaDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrudRestJpaDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/rahmasir/crudrestjpademo/CrudRestJpaDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.rahmasir.crudrestjpademo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudRestJpaDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
